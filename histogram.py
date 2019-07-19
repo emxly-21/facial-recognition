8e1e5c1c8f26b510ed185c98c746f15865d5fe42
David Qian <david.qian02@gmail.com>
2019-07-19 16:12:11 -0400

    edited histogram

--- histogram.py
+++ histogram.py
@@ -1,18 +1,13 @@
-def find_cutoff():
+def find_cutoff(db, pic):
     """
         Defines a local neighborhood and finds the local peaks
         in the spectrogram, which must be larger than the
         specified `amp_min`.
 
-
-        Returns
-        -------
-        List[Tuple[int, int]]
-            Time and frequency index-values of the local peaks in spectrogram.
-            Sorted by ascending frequency and then time.
+        Displays a plot
     """
-    db=np.load(stuff.npy) #your database
-    dq=np.load(david.npy) #your picture
+    db=np.load() #your database
+    dq=np.load() #your picture
     nodes=pairwise_dists(db,dq)
     fig, ax = plt.subplots()
     ax.hist(nodes,bin=len(nodes)//2,density=True)
