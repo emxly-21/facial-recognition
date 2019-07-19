4abc0dac82111137976e56f710aec4cf3832c0b8
Emily Y <emilygbs@yahoo.com>
2019-07-19 15:37:50 -0400

    updated near label method

--- near_label.py
+++ near_label.py
@@ -1,6 +1,3 @@
-import random
-from collections import Counter
-
 def near_labels(db):
     '''
     Performs label propagation through the network of nodes and clusters
@@ -20,13 +17,23 @@ def near_labels(db):
     '''
     counts = [len(db)] # keeps track of how many unique labels there are
     counter = 1
-    while not all(elem == counts[counter-1] for elem in counts[counter-5:]):
-        rand = random.randint(0, len(db))
+    for x in range(5):
+        rand = random.randint(0, len(db)-1)
         rand_node = db[rand]
         neighbors = rand_node.neighbors
         highest_count = Counter(neighbors).most_common()[0][0]
-        if rand_node.ID != highest_count:
-            rand_node.ID = highest_count
+        if rand_node.id != highest_count:
+            rand_node.id = highest_count
+            counts.append(counts[counter - 1] - 1)
+        else:
+            counts.append(counts[counter - 1])
+    while not all(elem == counts[counter-1] for elem in counts[counter-5:]):
+        rand = random.randint(0, len(db)-1)
+        rand_node = db[rand]
+        neighbors = rand_node.neighbors
+        highest_count = Counter(neighbors).most_common()[0][0]
+        if rand_node.id != highest_count:
+            rand_node.id = highest_count
             counts.append(counts[counter-1]-1)
         else:
             counts.append(counts[counter-1])
