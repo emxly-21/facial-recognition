6295b30d320229533c0b013227ac80946ced3218
Darshan <darsh797@gmail.com>
2019-07-19 16:17:21 -0400

    Added import statements to histogram.py

--- histogram.py
+++ histogram.py
@@ -1,13 +1,21 @@
+import numpy as np
+from create_edges import pairwise_dists
+import matplotlib.pyplot as plt
 def find_cutoff(db, pic):
     """
+
         Defines a local neighborhood and finds the local peaks
         in the spectrogram, which must be larger than the
         specified `amp_min`.
 
+        :parameter
+            db: .npy file that contains the database to be used
+            pic: .npy file containing the picture to be used.
+
         Displays a plot
     """
-    db=np.load() #your database
-    dq=np.load() #your picture
+    db=np.load(db) #your database
+    dq=np.load(pic) #your picture
     nodes=pairwise_dists(db,dq)
     fig, ax = plt.subplots()
     ax.hist(nodes,bin=len(nodes)//2,density=True)
