74a621b9101291cb36d13105533a59b9fe29a298
David Qian <david.qian02@gmail.com>
2019-07-30 16:29:38 -0400

    capstone week 2 original main

--- main2.py
+++ main2.py
@@ -9,11 +9,11 @@ import pickle
 # run this cell to download the models from dlib
 from dlib_models import download_model, download_predictor, load_dlib_models
 from dlib_models import models
-def main(database):
+def main():
     namereturn=''
     descreturn=np.array([])
-    ##download_model()
-    ##download_predictor()
+    download_model()
+    download_predictor()
 
 
 # take the picture
@@ -21,44 +21,44 @@ def main(database):
 
 
 # first, we load the models that dlib has to detect faces.
-    ##load_dlib_models()
+    load_dlib_models()
     face_detect = models["face detect"]
     face_rec_model = models["face rec"]
     shape_predictor = models["shape predict"]
 
 # detects the face through corners
     detections = list(face_detect(pic))
-# print(detections)  # list of shape n for n faces
+#print(detections)  # list of shape n for n faces
 
-    ##fig, ax = plt.subplots()
-    ##ax.imshow(pic)
+    fig, ax = plt.subplots()
+    ax.imshow(pic)
 
-    ##database = {}
+    database = {}
 
 
-    ##with open("database.pkl", mode="rb") as opened_file:
-        ##database = pickle.load(opened_file)
+    with open("database.pkl", mode="rb") as opened_file:
+        database = pickle.load(opened_file)
         #print("DATABASE")
         #print(database)
 
 
 
     print("Number of faces detected: {}".format(len(detections)))
-    ##for k, d in enumerate(detections):
-        # Get the landmarks/parts for the face in box d.
-        ##shape = shape_predictor(pic, d)
+    for k, d in enumerate(detections):
+        #Get the landmarks/parts for the face in box d.
+        shape = shape_predictor(pic, d)
         # Draw the face landmarks on the screen.
-        ##for i in range(68):
-            ##ax.plot(shape.part(i).x, shape.part(i).y, '+', color="blue")
+        for i in range(68):
+            ax.plot(shape.part(i).x, shape.part(i).y, '+', color="blue")
 
     import matplotlib.patches as patches
 
-    ##for faces in detections:
+    for faces in detections:
         # Create a Rectangle patch
-        ##rect = patches.Rectangle((faces.left(), faces.bottom()), faces.width(), -faces.height(), linewidth=1, edgecolor='g',
-        ##                         facecolor='none')
+        rect = patches.Rectangle((faces.left(), faces.bottom()), faces.width(), -faces.height(), linewidth=1, edgecolor='g',
+                                 facecolor='none')
         # Add the patch to the Axes
-        ##ax.add_patch(rect)
+        ax.add_patch(rect)
 
     names = {}
     database["Unknown Counter"]=0
@@ -68,7 +68,7 @@ def main(database):
         # let's take a look as to what the descriptor is!!
         shape = shape_predictor(pic, face)
         descriptor = np.array(face_rec_model.compute_face_descriptor(pic, shape))
-
+        #print(descriptor)
         # compares descriptor to database through img_in_database
         cutoff = .4
         #print("DATABASE")
@@ -80,14 +80,15 @@ def main(database):
             database["Unknown Counter"] += 1
 
         # plots name underneath square
-        ##ax.text(face.left()+(0.25*faces.width()), face.bottom()+(0.2*faces.height()), name, bbox=dict(facecolor='green', alpha=0.5))
+        ax.text(face.left()+(0.25*faces.width()), face.bottom()+(0.2*faces.height()), name, bbox=dict(facecolor='green', alpha=0.5))
 
         # adds to names dictionary
         names[name] = descriptor
         descreturn = descriptor
-    ##plt.show()
+        print(descreturn)
+    plt.show()
 
-    add_profile = "y"#input("Would you like to add this picture to the database? [y/n]  ")
+    add_profile = input("Would you like to add this picture to the database? [y/n]  ")
     if add_profile == "y":
         for name in names:
         # updates or creates a profile
